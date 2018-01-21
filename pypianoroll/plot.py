13b79fbc37c3067820333f88eb8a0da92cf46541
salu133445 <b02901080@ntu.edu.tw>
2018-01-21 21:27:55 +0800

    Fix label arguments

--- pypianoroll/plot.py
+++ pypianoroll/plot.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 def plot_pianoroll(ax, pianoroll, lowest=0, is_drum=False, beat_resolution=None,
                    downbeats=None, preset='default', cmap='Blues',
-                   xtick='bottom', ytick='left', xticklabel='auto',
+                   tick_loc=None, xtick='auto', ytick='octave', xticklabel='on',
                    yticklabel='auto', direction='in', label='both', grid='both',
                    grid_linestyle=':', grid_linewidth=.5):
     """
@@ -43,22 +43,24 @@ def plot_pianoroll(ax, pianoroll, lowest=0, is_drum=False, beat_resolution=None,
     cmap :  `matplotlib.colors.Colormap`
         Colormap to use in :func:`matplotlib.pyplot.imshow`. Default to
         'Blues'. Only effective when `pianoroll` is 2D.
-    xtick : {'bottom', 'top', 'both', 'off'}
-        Put ticks along x-axis at top, bottom, both or neither. Default to
-        'bottom'.
-    ytick : {'left', 'right', 'both', 'off'}
-        Put ticks along y-axis at left, right, both or neither. Default to
-        'left'.
-    xticklabel : {'auto', 'beat', 'step', 'none'}
-        Use beat number, step number or neither as tick labels along the
-        x-axis, or automatically set to 'beat' when `beat_resolution` is
-        given and set to 'step', otherwise. Default to 'auto'. Only
-        effective when `xtick` is not 'off'.
-    yticklabel : {'auto', 'octave', 'name', 'number', 'none'}
-        Use octave name, pitch name or pitch number or none as tick labels
-        along the y-axis, or automatically set to 'octave' when `is_drum` is
-        False and set to 'name', otherwise. Default to 'auto'. Only
-        effective when `ytick` is not 'off'.
+    tick_loc : tuple or list
+        List of locations to put ticks. Availables elements are 'bottom',
+        'top', 'left' and 'right'. If None, default to ('bottom', 'left').
+    xtick : {'auto', 'beat', 'step', 'off'}
+        Use beat number or step number as ticks along the x-axis, or
+        automatically set to 'beat' when `beat_resolution` is given and set
+        to 'step', otherwise. Default to 'auto'.
+    ytick : {'octave', 'pitch', 'off'}
+        Use octave or pitch as ticks along the y-axis. Default to 'octave'.
+    xticklabel : {'on', 'off'}
+        Indicate whether to add tick labels along the x-axis. Only effective
+        when `xtick` is not 'off'.
+    yticklabel : {'auto', 'name', 'number', 'off'}
+        If 'name', use octave name and pitch name (key name when `is_drum`
+        is   True) as tick labels along the y-axis. If 'number', use pitch
+        number. If 'auto', set to 'name' when `ytick` is 'octave' and
+        'number' when `ytick` is 'pitch'. Default to 'auto'. Only effective
+        when `ytick` is not 'off'.
     direction : {'in', 'out', 'inout'}
         Put ticks inside the axes, outside the axes, or both. Default to
         'in'. Only effective when `xtick` and `ytick` are not both 'off'.
@@ -75,28 +77,25 @@ def plot_pianoroll(ax, pianoroll, lowest=0, is_drum=False, beat_resolution=None,
     """
     if pianoroll.ndim not in (2, 3):
         raise ValueError("`pianoroll` must be a 2D or 3D numpy array")
-    if xtick not in ('bottom', 'top', 'both', 'off'):
-        raise ValueError("`xtick` must be one of {'bottom', 'top', 'both', "
-                         "'off'}")
-    if ytick not in ('left', 'right', 'both', 'off'):
-        raise ValueError("`ytick` must be one of {'left', 'right', 'both', "
-                         "'off'}")
-    if xticklabel not in ('auto', 'beat', 'step', 'off'):
-        raise ValueError("`xticklabel` must be one of {'auto', 'beat', 'step', "
+    if xtick not in ('auto', 'beat', 'step', 'off'):
+        raise ValueError("`xtick` must be one of {'auto', 'beat', 'step', "
                          "'none'}")
-    if xticklabel is 'beat' and beat_resolution is None:
-        raise ValueError("`beat_resolution` must be a number when `xticklabel` "
+    if xtick is 'beat' and beat_resolution is None:
+        raise ValueError("`beat_resolution` must be a number when `xtick` "
                          "is 'beat'")
-    if yticklabel not in ('auto', 'octave', 'name', 'number', 'off'):
-        raise ValueError("`yticklabel` must be one of {'auto', 'octave', "
-                         "'name', 'number', 'none'}")
+    if ytick not in ('octave', 'pitch', 'off'):
+        raise ValueError("`ytick` must be one of {octave', 'pitch', 'off'}")
+    if xticklabel not in ('on', 'off'):
+        raise ValueError("`xticklabel` must be 'on' or 'off'")
+    if yticklabel not in ('auto', 'name', 'number', 'off'):
+        raise ValueError("`yticklabel` must be one of {'auto', 'name', "
+                         "'number', 'off'}")
     if direction not in ('in', 'out', 'inout'):
         raise ValueError("`direction` must be one of {'in', 'out', 'inout'}")
     if label not in ('x', 'y', 'both', 'off'):
         raise ValueError("`label` must be one of {'x', 'y', 'both', 'off'}")
     if grid not in ('x', 'y', 'both', 'off'):
         raise ValueError("`grid` must be one of {'x', 'y', 'both', 'off'}")
-
 
     # plotting
     if pianoroll.ndim > 2:
@@ -109,15 +108,12 @@ def plot_pianoroll(ax, pianoroll, lowest=0, is_drum=False, beat_resolution=None,
               interpolation='none', extent=extent, origin='lower')
 
     # tick setting
-    if xticklabel == 'auto' and beat_resolution is not None:
-        xticklabel = 'beat'
-    else:
-        xticklabel = 'step'
-
-    if yticklabel == 'auto' and is_drum:
-        yticklabel = 'name'
-    else:
-        yticklabel = 'octave'
+    if tick_loc is None:
+        tick_loc = ('bottom', 'left')
+    if xtick == 'auto':
+        xtick = 'beat' if beat_resolution is not None else 'step'
+    if yticklabel == 'auto':
+        yticklabel = 'name' if ytick == 'octave' else 'number'
 
     if preset == 'plain':
         ax.axis('off')
@@ -126,17 +122,13 @@ def plot_pianoroll(ax, pianoroll, lowest=0, is_drum=False, beat_resolution=None,
                        right='off', labelbottom='off', labeltop='off',
                        labelleft='off', labelright='off')
     else:
-        bottom = 'on' if xtick == 'bottom' or xtick == 'both' else 'off'
-        top = 'on' if xtick == 'top' or xtick == 'both' else 'off'
-        left = 'on' if ytick == 'left' or ytick == 'both' else 'off'
-        right = 'on' if ytick == 'right' or ytick == 'both' else 'off'
+        labelbottom = 'on' if xticklabel != 'off' else 'off'
+        labelleft = 'on' if yticklabel != 'off' else 'off'
 
-        labelbottom = 'off' if xticklabel == 'none' else 'on'
-        labelleft = 'off' if yticklabel == 'none' else 'on'
-
-        ax.tick_params(direction=direction, bottom=bottom, top=top, left=left,
-                       right=right, labelbottom=labelbottom, labeltop='off',
-                       labelleft=labelleft, labelright='off')
+        ax.tick_params(direction=direction, bottom=('bottom' in tick_loc),
+                       top=('top' in tick_loc), left=('left' in tick_loc),
+                       right=('right' in tick_loc), labelbottom=labelbottom,
+                       labeltop='off', labelleft=labelleft, labelright='off')
 
     # x-axis
     if xticklabel == 'beat':
@@ -152,19 +144,21 @@ def plot_pianoroll(ax, pianoroll, lowest=0, is_drum=False, beat_resolution=None,
 
     # y-axis
     highest = lowest + pianoroll.shape[1] - 1
-    if yticklabel == 'octave':
+    if ytick == 'octave':
         ytick_first = lowest + 12 - lowest%12
         ax.set_yticks(np.arange(ytick_first, highest, 12))
-        ax.set_yticklabels(['C{}'.format(octave-2)
-                            for octave in range(ytick_first//12, highest//12)])
-    elif yticklabel == 'name':
+        if yticklabel == 'name':
+            ax.set_yticklabels(['C{}'.format(octave-2) for octave
+                                in range(ytick_first//12, highest//12)])
+    elif ytick == 'step':
         ax.set_yticks(np.arange(lowest, highest))
-        if is_drum:
-            ax.set_yticklabels([pretty_midi.note_number_to_drum_name(i)
-                                for i in range(lowest, highest)])
-        else:
-            ax.set_yticklabels([pretty_midi.note_number_to_name(i)
-                                for i in range(lowest, highest)])
+        if yticklabel == 'name':
+            if is_drum:
+                ax.set_yticklabels([pretty_midi.note_number_to_drum_name(i)
+                                    for i in range(lowest, highest)])
+            else:
+                ax.set_yticklabels([pretty_midi.note_number_to_name(i)
+                                    for i in range(lowest, highest)])
 
     # axis labels
     if label == 'x' or label == 'both':
