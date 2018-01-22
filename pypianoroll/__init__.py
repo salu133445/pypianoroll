800ef25c7e44dfff7742d279ee2549b73a25bd5f
salu133445 <b02901080@ntu.edu.tw>
2018-01-23 06:12:06 +0800

    Add expand()

--- pypianoroll/__init__.py
+++ pypianoroll/__init__.py
@@ -12,6 +12,6 @@ Features:
 from .track import Track
 from .multitrack import Multitrack
 from .pypianoroll import (is_pianoroll, is_standard_pianoroll, binarize, clip,
-                          compress_to_active, copy, plot, transpose,
+                          compress_to_active, copy, expand, plot, transpose,
                           trim_trailing_silence)
 from .plot import (plot_pianoroll)
