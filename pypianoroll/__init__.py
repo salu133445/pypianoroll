8a216f00cfd26ce5e16c33c6a9394afdd81ced7b
salu133445 <salu133445@gmail.com>
2018-01-27 18:39:04 +0800

    Change module name, add write() and update features

--- pypianoroll/__init__.py
+++ pypianoroll/__init__.py
@@ -3,18 +3,18 @@ Pypianoroll
 ===========
 A python package for handling multi-track piano-rolls.
 
-
 Features
 ========
-- handle multi-track piano-rolls with metadata
-- utilities to manipulate multi-track piano-rolls
-- save/load with efficient sparse matrix representation
-- write to MIDI file
+- handle piano-rolls of multiple tracks with metadata
+- utilities for manipulating piano-rolls
+- save to and load from .npz files using efficient sparse matrix format
+- parse from and write to MIDI file
+
 """
 from .version import __version__
 from .track import Track
 from .multitrack import Multitrack
-from .pypianoroll import (is_pianoroll, binarize, clip, copy, load, pad,
-                          pad_to_same, parse, plot, transpose,
-                          trim_trailing_silence)
+from .utilities import (is_pianoroll, binarize, clip, copy, load, pad,
+                        pad_to_same, parse, plot, save, transpose,
+                        trim_trailing_silence, write)
 from .plot import (plot_pianoroll, save_animation)
