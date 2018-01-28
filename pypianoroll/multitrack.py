8b92302efdd7cbb35b31d5a10c8fa36f2d54c89e
salu133445 <salu133445@gmail.com>
2018-01-28 16:08:41 +0800

    Fix downbeat array checking in save()

--- pypianoroll/multitrack.py
+++ pypianoroll/multitrack.py
@@ -1174,7 +1174,7 @@ class Multitrack(object):
         info_dict = {'beat_resolution': self.beat_resolution,
                      'name': self.name}
 
-        if self.downbeat:
+        if self.downbeat is not None:
             array_dict['downbeat'] = self.downbeat
 
         for idx, track in enumerate(self.tracks):
