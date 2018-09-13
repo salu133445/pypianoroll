7af18a46ddb7820b336e6018a7a845d9556b8d89
salu133445 <salu133445@gmail.com>
2018-09-14 03:39:47 +0800

    Check working directory before everything

--- doc/update.sh
+++ doc/update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Script for building and updating the current documentation
+if [ ! -d source ]; then
+    echo "Source directory not found. Probably in the wrong working directory."
+    exit 1
+fi
 rm -r ./build
 make html
 rm -r ../docs/*
