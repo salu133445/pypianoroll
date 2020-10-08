56636fcfbcb82bff8b7d041f29a91faf464d5ae2
Hao-Wen Dong <salu.hwdong@gmail.com>
2020-10-08 09:34:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,52 +1,59 @@
 """Setup script."""
-import os.path
-from codecs import open
+from pathlib import Path
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
-with open(
-    os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"),
-    encoding="utf-8",
-) as f:
-    LONG_DESCRIPTION = f.read()
 
-VERSION = {}
-with open(os.path.join("pypianoroll", "version.py")) as f:
-    exec(f.read(), VERSION)
+def _get_long_description():
+    with open(str(Path(__file__).parent / "README.md"), "r") as f:
+        return f.read()
+
+
+def _get_version():
+    with open(str(Path(__file__).parent / "pypianoroll/version.py"), "r") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                delimeter = '"' if '"' in line else "'"
+                return line.split(delimeter)[1]
+    raise RuntimeError("Cannot read version string.")
+
+
+VERSION = _get_version()
 
 setup(
     name="pypianoroll",
-    version=VERSION["__version__"],
+    version=VERSION,
     author="Hao-Wen Dong",
     author_email="salu.hwdong@gmail.com",
     description="A toolkit for working with piano rolls",
-    long_description=LONG_DESCRIPTION,
+    long_description=_get_long_description(),
     long_description_content_type="text/markdown",
     download_url=(
-        "https://github.com/salu133445/pypianoroll/archive/v"
-        + VERSION["__version__"]
-        + ".tar.gz"
+        f"https://github.com/salu133445/pypianoroll/archive/v{VERSION}.tar.gz"
     ),
     project_urls={
         "Documentation": "https://salu133445.github.io/pypianoroll/"
     },
+    license="MIT",
     keywords=["music", "audio", "music-information-retrieval"],
-    packages=["pypianoroll"],
+    packages=find_packages(
+        include=["pypianoroll", "pypianoroll.*"], exclude=["tests"]
+    ),
     install_requires=[
-        "six>=1.0.0,<2.0",
-        "numpy>=1.10.0,<2.0",
-        "scipy>=1.0.0,<2.0",
-        "pretty_midi>=0.2.8,<1.0",
+        "numpy>=1.10.0",
+        "scipy>=1.0.0",
+        "pretty_midi>=0.2.8",
         "matplotlib>=1.5",
     ],
     extras_require={
         "animation": ["moviepy>=0.2.3.2"],
-        "test": ["pytest>=4.6", "pytest-cov>=2.0"],
+        "test": ["pytest>=6.0", "pytest-cov>=2.0"],
     },
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Topic :: Multimedia :: Sound/Audio",
     ],
+    python_requires=">=3.6",
 )
