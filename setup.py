55c98361e64b7227bd77948172158ff6edb03c3a
George Ogden <george@goprogram.ai>
2022-12-19 11:55:07 +0000

    change numpy dependency

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         include=["pypianoroll", "pypianoroll.*"], exclude=["tests"]
     ),
     install_requires=[
-        "numpy>=1.12.0",
+        "numpy>=1.12.0,<1.24",
         "scipy>=1.0.0",
         "pretty_midi>=0.2.8",
         "matplotlib>=1.5",
